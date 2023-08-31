daf49c36cbd6e38f0593115240083c484d406d7e
Alex <pr9nek@mail.ru>
2023-08-31 11:57:18 +0400

    fix: поправил header, feat: добавил Tabs

--- src/components/App-Header/App-Header.jsx
+++ src/components/App-Header/App-Header.jsx
@@ -6,10 +6,10 @@ import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 
 export default function AppHeader() {
     return (
-        <div className={HeaderStyles.global}>
+        <header className={HeaderStyles.header}>
             <div className={`${HeaderStyles.header_container} pb-4 pt-4`}>
                 
-                <div className={HeaderStyles.left_menu}>
+                <div className={HeaderStyles.header_item}>
                     <Nav>
                         <BurgerIcon type="primary" />
                         <span className="text text_type_main-default pl-2">Конструктор</span>
@@ -20,17 +20,19 @@ export default function AppHeader() {
                     </Nav>
                 </div>
 
-                <div className='mt-2 mb-2'> 
-                    <Logo/>
-                </div>   
+                <div className={`${HeaderStyles.header_item} jc-c`}>
+                    <div className='mt-2 mb-2'> 
+                        <Logo/>
+                    </div>
+                </div>
 
-                <div className={HeaderStyles.right_menu}>
+                <div className={`${HeaderStyles.header_item} jc-f-e`}>
                     <Nav>
                     <ProfileIcon type="secondary"/>
                     <p className="text text_type_main-default text_color_inactive pl-2">Лента заказов</p>
                     </Nav>
                 </div>
             </div>
-        </div>
+        </header>
     )
 }
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,22 +1,24 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
 import AppHeader from "../App-Header/App-Header";
+import Tabs from "../Burger-Ingredients/Tabs/Tabs";
 
 function App() {
   return (
     <div className={styles.app}>
       <AppHeader />
-      <div className={styles.global}>
+      <main className={`${styles.main} pl-10 pr-10`}>
         <div className={styles.container}>
           <p className="text text_type_main-large pb-5">
           Соберите бургер
           </p>
+          <Tabs />
         </div>
 
         <div className={styles.container}>
           
         </div>
-      </div>
+      </main>
     </div>
   );
 }
