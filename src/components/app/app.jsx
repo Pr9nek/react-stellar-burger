a40cba4b24c0d9ee81a532f60bc448f902da86da
Alex <pr9nek@mail.ru>
2023-10-31 10:25:51 +0400

    refact: перенёс разметку,отвечающую за вывод BurgerIngredients и BurgerConstructor, в компонент Home

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,44 +1,20 @@
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { useEffect } from "react";
-import { useSelector, useDispatch } from 'react-redux';
+// import { useEffect } from "react";
+// import { useSelector, useDispatch } from 'react-redux';
+import { Route, Routes } from "react-router-dom";
 import styles from "./app.module.css";
 import AppHeader from "../app-header/app-header";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import {getIngredients} from '../../services/actions/ingredients/actions';
+import Home from "../../pages/home/home";
 
 function App() {
-
- const { isLoading, Error, ingredients } = useSelector(store => store.ingredients);
- const dispatch = useDispatch();
-
-  useEffect(()=> {
-    dispatch(getIngredients());
-}, [])
 
   return (
     <div className={styles.app}>
       <AppHeader />
-          <main className={`${styles.main} pl-5 pr-5`}>
-          <DndProvider backend={HTML5Backend}>
-            <section className={`${styles.section} pb-10`}>
-              {isLoading && 'Загрузка...'}
-              {Error && 'Произошла ошибка'}
-              {!isLoading &&
-                !Error &&
-                ingredients !== null && <BurgerIngredients />}
-            </section>
-
-            <section className={`${styles.section} pt-15 pl-4 pb-10`}>
-              {isLoading && 'Загрузка...'}
-              {Error && 'Произошла ошибка'}
-              {!isLoading &&
-                !Error &&
-                ingredients !== null && <BurgerConstructor />}
-            </section>
-            </DndProvider>
-          </main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={<OnlyUnAuth component={<Login />} />} />
+        <Route path="/profile" element={<OnlyAuth component={<Profile />} />} /> */}
+      </Routes>
     </div>
   );
 }
