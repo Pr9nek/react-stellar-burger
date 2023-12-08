c7fada9006f9ebf6a0d1d121063482b8f5dde049
Alex <pr9nek@mail.ru>
2023-12-08 18:04:29 +0400

    refact: типизация useForme

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -3,7 +3,7 @@ import { useState } from 'react';
 export function useForm(inputValues={}) {
     const [values, setValues] = useState(inputValues);
   
-    const handleChange = (event) => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
       const {value, name} = event.target;
       setValues({...values, [name]: value});
     };
